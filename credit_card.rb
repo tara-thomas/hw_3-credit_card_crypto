4e4223d8f577d872100934cf98de0c25a47a50be
tara-thomas <t.tarathomas@gmail.com>
2021-03-18 19:02:37 +0800

    Initialized Credit Card variables and completed Luhn Algorithm

--- credit_card.rb
+++ credit_card.rb
@@ -1,14 +1,18 @@
-require_relative './luhn_validator.rb'
+require_relative './luhn_validator'
 require 'json'
 
 class CreditCard
   # TODO: mixin the LuhnValidator using an 'include' statement
-
+  include LuhnValidator
   # instance variables with automatic getter/setter methods
   attr_accessor :number, :expiration_date, :owner, :credit_network
 
   def initialize(number, expiration_date, owner, credit_network)
     # TODO: initialize the instance variables listed above
+    @number = number
+    @expiration_date = expiration_date
+    @owner = owner
+    @credit_network = credit_network
   end
 
   # returns json string
